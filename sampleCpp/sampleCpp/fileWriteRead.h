4d1024ec6421faa847e69bc4ae1f82c042a92bc5
ichitomo <ichitomo0421@gmail.com>
2015-08-28 18:03:22 +0900

    ファイルにデータを書き込むプログラムの作成

---
+++ sampleCpp/sampleCpp/fileWriteRead.h
@@ -0,0 +1,14 @@
+//
+//  fileWriteRead.h
+//  sampleCpp
+//
+//  Created by 一ノ瀬智太 on 2015/08/28.
+//  Copyright (c) 2015年 ichinose.tomohiro. All rights reserved.
+//
+
+#ifndef __sampleCpp__fileWriteRead__
+#define __sampleCpp__fileWriteRead__
+
+#include <stdio.h>
+
+#endif /* defined(__sampleCpp__fileWriteRead__) */
