936b5d51eacacc3a7d588b4a8a07fdeb17ccf412
ichitomo <ichitomo0421@gmail.com>
2015-08-28 16:33:19 +0900

    sampleCppを追加

---
+++ sampleCpp/sampleCpp/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  sampleCpp
+//
+//  Created by 一ノ瀬智太 on 2015/08/28.
+//  Copyright (c) 2015年 ichinose.tomohiro. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    char c;     //１文字を格納する変数
+    string s;   //文字列を格納する変数
+    //データを格納する
+    c = 'A';
+    s = "hello";
+    //データを表示する
+    cout << c << "\n";
+    cout << s << "\n";
+    return 0;
+}
